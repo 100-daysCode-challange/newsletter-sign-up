6a6f3e4a3e8470de38ef53fe710a2a3102c85c46
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-09-11 15:42:19 +0200

    n

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const formEmail = document.querySelector(".email");
-const formInput = document.querySelector(".form-input");
-const formButton = document.querySelector(".form-btn");
+// validating the email address.
 
+function emailValidation() {
+    const mail = document.querySelector(".email").value;
+
+    var regx = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (regx.test(mail)) {
+      alert("Thanks for subscribing!");
+      return true;
+    } else {
+      alert("wrong email");
+      return false;
+    }
+}
