27918a057b649549667fbbe3951d48ff067667d4
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-09-12 09:48:06 +0200

    js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // validating the email address.
 
 function emailValidation() {
+    const formContainer = document.querySelector(".form-container");
+    const notification = document.querySelector(".notification");
     const email = document.getElementById("email").value;
     const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
 
     if (email.match(pattern)) {
-      alert("Thanks for subscribing!");
-      return true;
+        formContainer.style.display = "none";
+        notification.style.display = "block";
+        return true;
     } else {
       alert("wrong email");
       return false;
