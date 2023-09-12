cc7b87042ae6de34dfb15a9b3a60c7664074ece2
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-09-12 21:59:20 +0200

    n

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // validating the email address.
 
 function emailValidation() {
-    const formContainer = document.querySelector(".main-container");
-    const notification = document.querySelector(".notification");
-    const emailInput = document.getElementById("email");
-    const email = emailInput.value;
-    const errorMessage = document.querySelector(".error-message");
-    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+  const formContainer = document.querySelector(".main-container");
+  const notification = document.querySelector(".notification");
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value;
+  const errorMessage = document.querySelector(".error-message");
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (email.match(pattern)) {
-        formContainer.style.display = "none";
-        notification.style.display = "block";
-        return true;
-    } else {
-      emailInput.style.borderColor = "hsl(4, 100%, 67%)";
-      emailInput.style.borderWidth = "2px"
-      emailInput.style.backgroundColor = "hsla(4, 100%, 67%, 0.5)";
-      errorMessage.style.display = "block"
-      return false;
-    }
+  if (email.match(pattern)) {
+    formContainer.style.display = "none";
+    notification.style.display = "block";
+    return true;
+  } else {
+    emailInput.style.borderColor = "hsl(4, 100%, 67%)";
+    emailInput.style.borderWidth = "2px";
+    emailInput.style.backgroundColor = "hsla(4, 100%, 67%, 0.5)";
+    errorMessage.style.display = "block";
+    return false;
+  }
 }
 
-function returnToMainPage(){
-    window.location.href = "index.html"
+function returnToMainPage() {
+  window.location.href = "index.html";
 }
